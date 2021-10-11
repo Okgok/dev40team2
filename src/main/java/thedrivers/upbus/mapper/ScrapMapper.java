e5c5cf074801799df8497b2f4621035beaa0fc1b
xkxltm12 <xkxltm12@naver.com>
2021-10-11 17:44:30 +0900

    [개인위탁판매재고 MemberSellManage에서 개인위탁판매신청코드 상품소분류코드 추가 ,어드민페이지 좌측 네비에 업사이클링 재료 입고 관리 추가 ,업사이클링 재료 재고 디비랑 연결 ,업사이클링 재료 매입 디비랑 연결 밑 AJAX 작업 완료 ,업사이클링 신청서 리스트랑 상세페이지 500오류있음 디비 이름이 바껴서 바꾸다 어딘가 잘못된거같음 내일 문제 찾아서 해결하겠음 ]

--- src/main/java/thedrivers/upbus/mapper/ScrapMapper.java
+++ src/main/java/thedrivers/upbus/mapper/ScrapMapper.java
@@ -5,6 +5,8 @@ import java.util.List;
 
 import org.apache.ibatis.annotations.Mapper;
 
+import thedrivers.upbus.domain.ScrapListInventory;
+import thedrivers.upbus.domain.ScrapSale;
 import thedrivers.upbus.domain.ScrapSaleRequest;
 
 @Mapper
@@ -13,10 +15,12 @@ public interface ScrapMapper {
 	public List<ScrapSaleRequest> getScrapSale();
 	// 1-2 업사이클링 재료 판매 신청서 상세 조회
 	public ScrapSaleRequest getScrapSaleDetail(String scrapSaleRequestCode);
-
-	
-	// 2.업사이클링 재료 판매 신청서 승인
-	
+	// 2.업사이클링 재료 판매 신청서 승인	
 	public int scrapSaleApprovalModify(ScrapSaleRequest scrapSaleRequest);
-	
+	// 3.업사이클링 재료 재고 리스트
+	public List<ScrapListInventory> getScrapList();
+	// 4.업사이클링 매입 확정 리스트
+	public List<ScrapSale> getSaleList();
+	// 5.업사이클링 매입 확정 버튼 승인
+	public int scrapSaleAmountApprovalModify(ScrapSale scrapSale);
 }
